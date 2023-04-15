0be227807186112e7044320ed293d414e6da3b0c
Jaswir <jaswirraghoe@gmail.com>
2023-04-15 23:53:05 +0200

    Progress with axios post

--- groceror-frontend/src/components/RegisterForm.tsx
+++ groceror-frontend/src/components/RegisterForm.tsx
@@ -4,72 +4,83 @@ import { useState } from "react";
 
 function RegisterForm({ setCount }: { setCount: any }) {
 
-    // const [post, setPost] = useState({
-    //     title: '',
-    //     body: ''
-    // })
+    interface User {
+        Username: string
+        Password: string
+    }
 
-    // const handleSubmit = async (e: any) => {
+    let initialState: User = {
+        Username: "",
+        Password: "",
+    }
 
-    //     e.preventDefault();
-    //     axios.post('https://jsonplaceholder.typicode.com/posts',
-    //         { 'dragons': 'shadowdragon' })
-    //         .then(response => {
+    const [username, setUserName] = useState<string>(initialState.Username)
+    const [password, setPassword] = useState<string>(initialState.Password)
+    const [user, setUser] = useState<User>(initialState)
 
-    //             console.log(response)
+    const handleInput = (event:HTMLInputElement) => {
+        setUser({...user, [event.id]: event.value})
+    }
 
-    //         }).catch(err => {
 
-    //             console.log(err)
+        const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
-    //         })
+            e.preventDefault();
+            console.log(user)
+            axios.post('https://jsonplaceholder.typicode.com/posts', {user})
+                .then(response => console.log(response.data))
+                .catch(err => console.log(err))
 
-    // }
+        }
 
-    return (
-        // <div className='grid h-screen w-full'>
-        //     <div className='bg-gray-800 flex flex-col justify-center'>
-        <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
-            <h2 className='text-4xl dark:text-white font-bold text-center'>Register</h2>
-            <div className='flex flex-col text-gray-400 py-2'>
-                <label>User Name:</label>
-                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+        return (
+            // <div className='grid h-screen w-full'>
+            //     <div className='bg-gray-800 flex flex-col justify-center'>
+            <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                <h2 className='text-4xl dark:text-white font-bold text-center'>Register</h2>
+                <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Username:</label>
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                    type="text" />
-            </div>
-            <div className='flex flex-col text-gray-400 py-2'>
-                <label>Password:</label>
-                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+                        type="text"
+                        id = "Username"
+                        onChange={(e) => handleInput(e.target)} />
+                </div>
+                <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Password:</label>
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                    type="password" />
-            </div>
-            <div className='flex flex-col text-gray-400 py-2'>
-                <label>Confirm Password:</label>
-                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+                        type="password"
+                        id = "Password"
+                        onChange={(e) => handleInput(e.target)} />
+                </div>
+                <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Confirm Password:</label>
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                    type="password" />
-            </div>
-            <br></br>
-            <button className='w-full py-2 bg-teal-500 hover:bg-teal-700 \
+                        type="password" />
+                </div>
+                <br></br>
+                <button className='w-full py-2 bg-teal-500 hover:bg-teal-700 \
              border-teal-500 hover:border-teal-700 text-sm border-4 \
               text-white py-1 px-2 rounded-lg'>
-                Sign Up
-            </button>
-            <p className=' text-gray-400 py-2'>Already registered?</p>
-            <p className=' text-gray-400 py-2 underline'>
-                <button className="underline"
-                    onClick={() => setCount(1)} type="button">
-                    Sign In
+                    Sign Up
                 </button>
-            </p>
-        </form>
-        //     </div>
-        // </div>
-    )
+                <p className=' text-gray-400 py-2'>Already registered?</p>
+                <p className=' text-gray-400 py-2 underline'>
+                    <button className="underline"
+                        onClick={() => setCount(1)} type="button">
+                        Sign In
+                    </button>
+                </p>
+            </form>
+            //     </div>
+            // </div>
+        )
 
 
 
 
-}
+    }
 
-export default RegisterForm
+    export default RegisterForm
