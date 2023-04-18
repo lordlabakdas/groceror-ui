cbb08478fb047e5961a61dfa9f6f51a7c3dd091a
Jaswir <jaswirraghoe@gmail.com>
2023-04-18 13:43:48 +0200

    Axios post demo that works

--- groceror-frontend/src/components/LoginForm.tsx
+++ groceror-frontend/src/components/LoginForm.tsx
@@ -4,70 +4,67 @@ import { useState } from "react";
 
 function LoginForm({ setCount }: { setCount: any }) {
 
-    const [post, setPost] = useState({
-        title: '',
-        body: ''
-    })
-
-    const handleInput = (event : any) => {
-        setPost({...post, [event.target.name]: event.target.event})
+    interface User {
+        Username: string
+        Password: string
     }
 
-    const handleSubmit = async (e: any) => {
+    let initialState: User = {
+        Username: "",
+        Password: "",
+    }
+
+    const [username, setUserName] = useState<string>(initialState.Username)
+    const [password, setPassword] = useState<string>(initialState.Password)
+    const [user, setUser] = useState<User>(initialState)
+
+    const handleInput = (event: HTMLInputElement) => {
+        setUser({ ...user, [event.id]: event.value })
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
         e.preventDefault();
-        axios.post('https://jsonplaceholder.typicode.com/posts',
-            { 'dragons': 'shadowdragon' })
-            .then(response => {
-
-                console.log(response)
-
-            }).catch(err => {
-
-                console.log(err)
-
-            })
+        console.log(user)
+        axios.post('https://jsonplaceholder.typicode.com/posts', { user })
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
 
     }
 
-
     return (
-        // <div className='grid h-screen w-full'>
-        //     <div className='bg-gray-800 flex flex-col justify-center'>
         <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
-            <h2 className='text-4xl dark:text-white font-bold text-center'>Sign In</h2>
+            <h2 className='text-4xl dark:text-white font-bold text-center'>Login</h2>
             <div className='flex flex-col text-gray-400 py-2'>
-                <label>User Name:</label>
+                <label>Username:</label>
                 <input className='rounded-lg bg-gray-700 mt-2 p-2 \
-              focus:order-blue focus:bg-gray-800 focus:outline-none'
+          focus:order-blue focus:bg-gray-800 focus:outline-none'
                     type="text"
-                    />
+                    id="Username"
+                    onChange={(e) => handleInput(e.target)} />
             </div>
             <div className='flex flex-col text-gray-400 py-2'>
                 <label>Password:</label>
                 <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
                     type="password"
-                   />
+                    id="Password"
+                    onChange={(e) => handleInput(e.target)} />
             </div>
             <br></br>
-
             <button className='w-full py-2 bg-teal-500 hover:bg-teal-700 \
              border-teal-500 hover:border-teal-700 text-sm border-4 \
-              text-white py-1 px-2 rounded-lg' type="button" >
-                Sign In
+              text-white py-1 px-2 rounded-lg'>
+                Sign Up
             </button>
-
-            {/* <p className=' text-gray-400 py-2'>Need an Account?</p>
+            <p className=' text-gray-400 py-2'>Need an Account?</p>
             <p className=' text-gray-400 py-2 underline'>
                 <button className="underline"
-                   >
+                    onClick={() => setCount(0)} type="button">
                     Sign Up
                 </button>
-            </p> */}
+            </p>
         </form>
-        //     </div>
-        // </div>
     )
 
 
--- groceror-frontend/src/components/RegisterForm.tsx
+++ groceror-frontend/src/components/RegisterForm.tsx
@@ -18,69 +18,65 @@ function RegisterForm({ setCount }: { setCount: any }) {
     const [password, setPassword] = useState<string>(initialState.Password)
     const [user, setUser] = useState<User>(initialState)
 
-    const handleInput = (event:HTMLInputElement) => {
-        setUser({...user, [event.id]: event.value})
+    const handleInput = (event: HTMLInputElement) => {
+        setUser({ ...user, [event.id]: event.value })
     }
 
 
-        const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
-            e.preventDefault();
-            console.log(user)
-            axios.post('https://jsonplaceholder.typicode.com/posts', {user})
-                .then(response => console.log(response.data))
-                .catch(err => console.log(err))
+        e.preventDefault();
+        console.log(user)
+        axios.post('https://jsonplaceholder.typicode.com/posts', { user })
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
 
-        }
+    }
 
-        return (
-            // <div className='grid h-screen w-full'>
-            //     <div className='bg-gray-800 flex flex-col justify-center'>
-            <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
-                <h2 className='text-4xl dark:text-white font-bold text-center'>Register</h2>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Username:</label>
-                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+    return (
+        <form onSubmit={handleSubmit} className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+            <h2 className='text-4xl dark:text-white font-bold text-center'>Register</h2>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <label>Username:</label>
+                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                        type="text"
-                        id = "Username"
-                        onChange={(e) => handleInput(e.target)} />
-                </div>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Password:</label>
-                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+                    type="text"
+                    id="Username"
+                    onChange={(e) => handleInput(e.target)} />
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <label>Password:</label>
+                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                        type="password"
-                        id = "Password"
-                        onChange={(e) => handleInput(e.target)} />
-                </div>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Confirm Password:</label>
-                    <input className='rounded-lg bg-gray-700 mt-2 p-2 \
+                    type="password"
+                    id="Password"
+                    onChange={(e) => handleInput(e.target)} />
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <label>Confirm Password:</label>
+                <input className='rounded-lg bg-gray-700 mt-2 p-2 \
               focus:order-blue focus:bg-gray-800 focus:outline-none'
-                        type="password" />
-                </div>
-                <br></br>
-                <button className='w-full py-2 bg-teal-500 hover:bg-teal-700 \
+                    type="password" />
+            </div>
+            <br></br>
+            <button className='w-full py-2 bg-teal-500 hover:bg-teal-700 \
              border-teal-500 hover:border-teal-700 text-sm border-4 \
               text-white py-1 px-2 rounded-lg'>
-                    Sign Up
+                Sign Up
+            </button>
+            <p className=' text-gray-400 py-2'>Already registered?</p>
+            <p className=' text-gray-400 py-2 underline'>
+                <button className="underline"
+                    onClick={() => setCount(1)} type="button">
+                    Sign In
                 </button>
-                <p className=' text-gray-400 py-2'>Already registered?</p>
-                <p className=' text-gray-400 py-2 underline'>
-                    <button className="underline"
-                        onClick={() => setCount(1)} type="button">
-                        Sign In
-                    </button>
-                </p>
-            </form>
-            //     </div>
-            // </div>
-        )
+            </p>
+        </form>
+    )
 
 
 
 
-    }
+}
 
-    export default RegisterForm
+export default RegisterForm
